183d8e22fdaa0512845ca736c5be1cb990e6cf9d
Sean Bigelow <sean.bigelow1@gmail.com>
2020-12-03 19:38:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,12 +17,16 @@ class Song
   end
   
   def self.new_by_filename(filename)
-  song = self.new(name)
-  song.save
-  return song
+   artist, song = filename.split(" - ")
+   new_song = self.new(song)
+   new_song.artist_name = artist
+   new_song
   end
   
- 
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
   
 end
 
